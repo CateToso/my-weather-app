e176aa447075c3b4e5203dc5df87d0474be2753b
Cate90 <cat.toso@gmail.com>
2023-09-10 23:13:24 +0100

    Create JS file displaying current date and time

---
+++ script.js
@@ -0,0 +1,24 @@
+let currentTime = new Date();
+let h3 = document.querySelector("h3");
+
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+let day = days[currentTime.getDay()];
+
+let hours = currentTime.getHours();
+if (hours < 10) {
+  hours = `0${hours}`;
+}
+let minutes = currentTime.getMinutes();
+if (minutes < 10) {
+  minutes = `0${minutes}`;
+}
+
+h3.innerHTML = `${day} ${hours}:${minutes}`;
